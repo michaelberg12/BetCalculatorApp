14680a5ac1715a177442ed3a5f0e9d4eded19cf6
Michael Berg <gala52210@yahoo.com>
2021-05-22 19:08:45 -0600

    OddsGetter Converted

--- app/src/main/java/com/csci448/bet_calc/utility/OddGetter.kt
+++ app/src/main/java/com/csci448/bet_calc/utility/OddGetter.kt
@@ -2,8 +2,5 @@ package com.csci448.bet_calc.utility
 
 //TODO build out this class
 interface OddGetter {
-    fun getOdds(): Double{
-        //implemented by children
-        return 0.0
-    }
+    fun getOdds(): Double
 }
---
+++ app/src/main/java/com/csci448/bet_calc/utility/odd_getters/PointsBet.kt
@@ -0,0 +1,11 @@
+package com.csci448.bet_calc.utility.odd_getters
+
+import com.csci448.bet_calc.utility.OddGetter
+
+class PointsBet: OddGetter {
+
+    override fun getOdds(): Double {
+        TODO("Not yet implemented")
+    }
+
+}
